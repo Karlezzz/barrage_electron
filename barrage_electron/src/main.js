d25472497ed0021382fe8583d115c0d40a76a4c0
karle <970587844@qq.com>
2023-08-25 01:25:08 +0800

    fix transports to websocket

--- barrage_electron/src/main.js
+++ barrage_electron/src/main.js
@@ -5,24 +5,25 @@ import router from './router/index.js'
 import echarts_ from './echart/index'
 import store from './store/index'
 import './api/request'
+import VueSocketIO from 'vue-socket.io'
 import SocketIO from 'socket.io-client'
 Vue.use(VueRouter)
 Vue.config.productionTip = false
 Vue.use(
-  new SocketIO({
+  new VueSocketIO({
     // 生产环境建议关闭，开发环境打开(在控制台看到socket连接和事件监听的信息)
-    debug: true, 
-    connection:'http://localhost:3001',
-    options:{
+    debug: true,
+    connection: SocketIO.connect('http://localhost:3001', {
       // 创建时是否自动连接 我们这里设定为false,在指定页面再开启
       autoConnect: false,
       // 路径(默认值：/socket.io/) 
       path: "/my-app/",
       // 目前有两种传输方式：HTTP long-polling（可简称：polling）、WebSocket
-      transports: ['polling'],
+      transports: ['websocket'],
       // 附加请求头（在服务器端的 socket.handshake.headers 对象中找到）
-      extraHeaders:{},
-    },
+      extraHeaders: {},
+    }),
+    // options: ,
   })
 )
 new Vue({
