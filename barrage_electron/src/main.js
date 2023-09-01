b2c34669563b09854bd1105a5510e58a63878ee8
karle <970587844@qq.com>
2023-09-01 21:21:59 +0800

    change way to ues socket.io

--- barrage_electron/src/main.js
+++ barrage_electron/src/main.js
@@ -9,23 +9,23 @@ import VueSocketIO from 'vue-socket.io'
 import SocketIO from 'socket.io-client'
 Vue.use(VueRouter)
 Vue.config.productionTip = false
-Vue.use(
-  new VueSocketIO({
-    // 生产环境建议关闭，开发环境打开(在控制台看到socket连接和事件监听的信息)
-    debug: true,
-    connection: SocketIO.connect('http://192.168.1.10:3001', {
-      // 创建时是否自动连接 我们这里设定为false,在指定页面再开启
-      autoConnect: false,
-      // 路径(默认值：/socket.io/) 
-      path: "/my-app/",
-      // 目前有两种传输方式：HTTP long-polling（可简称：polling）、WebSocket
-      transports: ['websocket'],
-      // 附加请求头（在服务器端的 socket.handshake.headers 对象中找到）
-      extraHeaders: {},
-    }),
-    // options: ,
-  })
-)
+// Vue.use(
+//   new VueSocketIO({
+//     // 生产环境建议关闭，开发环境打开(在控制台看到socket连接和事件监听的信息)
+//     debug: true,
+//     connection: SocketIO.connect('http://192.168.1.10:3001', {
+//       // 创建时是否自动连接 我们这里设定为false,在指定页面再开启
+//       autoConnect: false,
+//       // 路径(默认值：/socket.io/) 
+//       path: "/my-app/",
+//       // 目前有两种传输方式：HTTP long-polling（可简称：polling）、WebSocket
+//       transports: ['websocket'],
+//       // 附加请求头（在服务器端的 socket.handshake.headers 对象中找到）
+//       extraHeaders: {},
+//     }),
+//     // options: ,
+//   })
+// )
 new Vue({
   render: h => h(App),
   router,
--- barrage_electron/src/pages/Main/Barrage/Barrage.vue
+++ barrage_electron/src/pages/Main/Barrage/Barrage.vue
@@ -1,237 +1,263 @@
 <template>
-  <!-- Barrage.vue -->
-  <div class="barrage">
-    <div class="chatArea">
-      <div class="message">
-        <div v-for="(item, index) in messageList" :key="index" :class="{
-            leftMessage: item.name != myName,
-            rightMessage: item.name == myName,
-          }">
-          <div class="messageAuthor">{{ item.name }}</div>
-          <div class="messageWord">{{ item.msg }}</div>
-        </div>
-      </div>
-      <div class="input">
-        <input type="text" v-model="newMessage" @keyup.enter="sendMessage" />
-        <div class="emoji">
-          <img src="../image/emoji.png" alt="" />
-        </div>
-        <div class="send" @click="sendMessage">
-          <img src="../image/Send.png" alt="" />
-        </div>
-      </div>
-    </div>
-  </div>
+	<!-- Barrage.vue -->
+	<div class="barrage">
+		<div class="chatArea">
+			<div class="message">
+				<div
+					v-for="(item, index) in messageList"
+					:key="index"
+					:class="{
+						leftMessage: item.name != myName,
+						rightMessage: item.name == myName,
+					}"
+				>
+					<div class="messageAuthor">{{ item.name }}</div>
+					<div class="messageWord">{{ item.msg }}</div>
+				</div>
+			</div>
+			<div class="input">
+				<input
+					type="text"
+					v-model="newMessage"
+					@keyup.enter="sendMessage"
+				/>
+				<div class="emoji">
+					<img
+						src="../image/emoji.png"
+						alt=""
+					/>
+				</div>
+				<div
+					class="send"
+					@click="sendMessage"
+				>
+					<img
+						src="../image/Send.png"
+						alt=""
+					/>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-  import {
-    nanoid
-  } from "nanoid";
-  import io from 'socket.io-client';
-  const socket = io('http://192.168.1.10:3001');
-  export default {
-    name: "Barrage",
-    data() {
-      return {
-        myName: "admin",
-        myId: nanoid(),
-        newMessage: "",
-        messageList: this.$store.state.barrage_chat.messageList,
-        reConnectTimer: null,
-        heartBeatTimer: null,
-        ws: null,
-      };
-    },
-    methods: {
-      sendMessage() {
-        const newMsg = {
-          type: "chat",
-          msg: this.newMessage,
-          uid: this.myId,
-          rid: "1",
-          name: "admin",
-        };
-        try {
-          socket.emit('sendMsg', JSON.stringify(newMsg));
-        } catch (error) {
-          console.log(error);
-        }
-        this.newMessage = "";
-      },
+import { nanoid } from 'nanoid'
+import { io } from 'socket.io-client'
+import requests from '@/api/request'
 
-      initSocket() {
-        //一定要移除旧的message监听，否则会出现重复监听的状况
-        // socket.removeListener('message');
-        //这是移除所有监听
-        socket.removeAllListeners();
-        socket.on('broadcast', (data) => {
-          this.$store.dispatch('saveMessage', JSON.parse(data))
-        })
-      }
-    },
-    mounted() {
-      let div = document.querySelector(".message");
-      div.scrollTop = div.scrollHeight;
+export default {
+	name: 'Barrage',
+	data() {
+		return {
+			myName: 'admin',
+			myId: nanoid(),
+			newMessage: '',
+			socket: null,
+		}
+	},
+  computed: {
+    messageList() {
+      return this.$store.state.barrage_chat.messageList
+    }
+  },
+	methods: {
+		sendMessage() {
+			const newMsg = {
+				type: 'chat',
+				msg: this.newMessage,
+				uid: this.myId,
+				rid: '1',
+				name: 'admin',
+			}
+			try {
+				this.socket.emit('sendMsg', JSON.stringify(newMsg))
+			} catch (error) {
+				console.log(error)
+			}
+			this.newMessage = ''
+		},
 
-      this.initSocket()
-    },
-    watch: {
-      // 屏幕滚动始终在最后一条
-      messageList() {
-        this.$nextTick(() => {
-          let div = document.querySelector(".message");
-          div.scrollTop = div.scrollHeight;
-        });
-      },
-    },
-    beforeDestroy() {
-      socket.disconnect()
-    },
-  };
+		async initSocket() {
+			try {
+				const socketUrl = await this.getSocketUrl()
+				this.socket = io(socketUrl, {
+					transports: ['websocket'],
+				})
+				this.socket.removeAllListeners()
+				this.socket.on('broadcast', data => {
+					this.$store.dispatch('saveMessage', JSON.parse(data))
+				})
+			} catch (error) {
+				console.log(error)
+			}
+		},
+		async getSocketUrl() {
+			const result = await requests.get('/socket/url')
+			return result.data.data
+		},
+		initScroll() {
+			let div = document.querySelector('.message')
+			div.scrollTop = div.scrollHeight
+		},
+	},
+	mounted() {
+    this.initScroll()
+		this.initSocket()
+	},
+	watch: {
+		// 屏幕滚动始终在最后一条
+		messageList() {
+			this.$nextTick(() => {
+				let div = document.querySelector('.message')
+				div.scrollTop = div.scrollHeight
+			})
+		},
+	},
+}
 </script>
 
 <style scoped>
-  * {
-    user-select: text;
-  }
+* {
+	user-select: text;
+}
 
-  .barrage {
-    float: left;
-    width: 55%;
-    height: 90%;
-    /* background-color: yellow; */
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+.barrage {
+	float: left;
+	width: 55%;
+	height: 90%;
+	/* background-color: yellow; */
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
 
-  .barrage .chatArea {
-    width: 98%;
-    height: 98%;
-    background-color: #222325;
-    border-radius: 10px;
-  }
+.barrage .chatArea {
+	width: 98%;
+	height: 98%;
+	background-color: #222325;
+	border-radius: 10px;
+}
 
-  .barrage .chatArea .message {
-    width: 98%;
-    margin-left: 1%;
-    height: 88%;
-    overflow: scroll;
-    margin-top: 10px;
-  }
+.barrage .chatArea .message {
+	width: 98%;
+	margin-left: 1%;
+	height: 88%;
+	overflow: scroll;
+	margin-top: 10px;
+}
 
-  .barrage .chatArea .message .leftMessage {
-    width: 50%;
-    margin: 5px 0 10px 10px;
-    word-break: break-all;
-    white-space: pre-line;
-    margin-top: 10px;
-  }
+.barrage .chatArea .message .leftMessage {
+	width: 50%;
+	margin: 5px 0 10px 10px;
+	word-break: break-all;
+	white-space: pre-line;
+	margin-top: 10px;
+}
 
-  .barrage .chatArea .message .leftMessage .messageAuthor {
-    font-size: 90%;
-    color: #e1e1e3;
-    font-weight: 500;
-    letter-spacing: 1px;
-  }
+.barrage .chatArea .message .leftMessage .messageAuthor {
+	font-size: 90%;
+	color: #e1e1e3;
+	font-weight: 500;
+	letter-spacing: 1px;
+}
 
-  .barrage .chatArea .message .leftMessage .messageWord {
-    margin-top: 5px;
-    background-color: #494b4f;
-    border-radius: 20px;
-    padding: 10px 15px 10px 15px;
-    color: #e1e1e3;
-    font-size: 100%;
-    /* border: 1px solid rgba(152, 152, 152, 0.092); */
-    display: inline-block;
-  }
+.barrage .chatArea .message .leftMessage .messageWord {
+	margin-top: 5px;
+	background-color: #494b4f;
+	border-radius: 20px;
+	padding: 10px 15px 10px 15px;
+	color: #e1e1e3;
+	font-size: 100%;
+	/* border: 1px solid rgba(152, 152, 152, 0.092); */
+	display: inline-block;
+}
 
-  .barrage .chatArea .message .rightMessage {
-    width: 50%;
-    text-align: right;
-    margin: 0 10px 5px auto;
-    word-break: break-all;
-    white-space: pre-line;
-  }
+.barrage .chatArea .message .rightMessage {
+	width: 50%;
+	text-align: right;
+	margin: 0 10px 5px auto;
+	word-break: break-all;
+	white-space: pre-line;
+}
 
-  .barrage .chatArea .message .rightMessage .messageAuthor {
-    text-align: right;
-    margin-right: 5px;
-    font-size: 90%;
-    color: #e1e1e3;
-    font-weight: 500;
-    letter-spacing: 1px;
-  }
+.barrage .chatArea .message .rightMessage .messageAuthor {
+	text-align: right;
+	margin-right: 5px;
+	font-size: 90%;
+	color: #e1e1e3;
+	font-weight: 500;
+	letter-spacing: 1px;
+}
 
-  .barrage .chatArea .message .rightMessage .messageWord {
-    margin-top: 10px;
-    background-color: #ea7724;
-    border-radius: 20px;
-    padding: 10px 15px 10px 15px;
-    color: #e1e1e3;
-    font-size: 100%;
-    text-align: left;
-    /* border: 1px solid rgba(152, 152, 152, 0.092); */
-    display: inline-block;
-  }
+.barrage .chatArea .message .rightMessage .messageWord {
+	margin-top: 10px;
+	background-color: #ea7724;
+	border-radius: 20px;
+	padding: 10px 15px 10px 15px;
+	color: #e1e1e3;
+	font-size: 100%;
+	text-align: left;
+	/* border: 1px solid rgba(152, 152, 152, 0.092); */
+	display: inline-block;
+}
 
-  .barrage .chatArea .input {
-    width: 98%;
-    height: 9%;
-    display: flex;
-    align-items: center;
-    margin-left: 1%;
-  }
+.barrage .chatArea .input {
+	width: 98%;
+	height: 9%;
+	display: flex;
+	align-items: center;
+	margin-left: 1%;
+}
 
-  .barrage .chatArea .input input {
-    width: 80%;
-    height: 80%;
-    margin-left: 2%;
-    border: none;
-    border-bottom: 1px solid #ea7724;
-    background-color: #ffffff00;
-    color: #e1e1e3;
-    font-size: 17px;
-    letter-spacing: 1px;
-    padding-left: 20px;
-  }
+.barrage .chatArea .input input {
+	width: 80%;
+	height: 80%;
+	margin-left: 2%;
+	border: none;
+	border-bottom: 1px solid #ea7724;
+	background-color: #ffffff00;
+	color: #e1e1e3;
+	font-size: 17px;
+	letter-spacing: 1px;
+	padding-left: 20px;
+}
 
-  .barrage .chatArea .input input:focus {
-    border-bottom: 2px solid #ea7724;
-  }
+.barrage .chatArea .input input:focus {
+	border-bottom: 2px solid #ea7724;
+}
 
-  .barrage .chatArea .emoji {
-    margin-left: 1%;
-    width: 8%;
-    height: 80%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    background-color: #323337;
-    border-radius: 10px;
-  }
+.barrage .chatArea .emoji {
+	margin-left: 1%;
+	width: 8%;
+	height: 80%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	cursor: pointer;
+	background-color: #323337;
+	border-radius: 10px;
+}
 
-  .barrage .chatArea .emoji img {
-    width: 30px;
-    height: 30px;
-  }
+.barrage .chatArea .emoji img {
+	width: 30px;
+	height: 30px;
+}
 
-  .barrage .chatArea .send {
-    width: 8%;
-    height: 80%;
-    margin-left: 1%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    background-color: #323337;
-    border-radius: 10px;
-  }
+.barrage .chatArea .send {
+	width: 8%;
+	height: 80%;
+	margin-left: 1%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	cursor: pointer;
+	background-color: #323337;
+	border-radius: 10px;
+}
 
-  .barrage .chatArea .send img {
-    width: 30px;
-    height: 30px;
-  }
+.barrage .chatArea .send img {
+	width: 30px;
+	height: 30px;
+}
 </style>
