2a137858f546803735d6f1127c3d387b07c6af5e
karle <970587844@qq.com>
2023-09-06 22:45:37 +0800

    add class Connection

--- barrage_electron/lib/models/index.js
+++ barrage_electron/lib/models/index.js
@@ -1,12 +1,15 @@
 const { AxiosHelper } = require('./AxiosHelper')
 const { ClassRoom } = require('./ClassRoom')
+const {Connection} = require('./Connection')
 const { Message } = require('./Message')
 const { Response } = require('./Response')
 const { Room } = require('./Room')
 const { User } = require('./User')
+
 module.exports = {
   AxiosHelper,
   ClassRoom,
+  Connection,
   Message,
   Response,
   Room,
