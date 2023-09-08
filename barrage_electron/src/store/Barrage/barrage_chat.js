51456fea0ca457913d47bfa5a84e02a19e67e826
karle <970587844@qq.com>
2023-09-08 22:44:28 +0800

    add setMessage in barrage store

--- barrage_electron/src/store/Barrage/barrage_chat.js
+++ barrage_electron/src/store/Barrage/barrage_chat.js
@@ -1,20 +1,23 @@
-import {nanoid} from 'nanoid'
+import { nanoid } from 'nanoid'
 import { ipcRenderer, IpcRenderer } from 'electron'
-const state={
-    messageList:[]  
+const state = {
+  messageList: []
 }
-const mutations={
-    PUTMESSAGE(state,data){ 
-        state.messageList.push(data)  
-        ipcRenderer.send('sendVuexMsg',data)
-    } 
+const mutations = {
+  PUTMESSAGE(state, data) {
+    state.messageList.push(data)
+    ipcRenderer.send('sendVuexMsg', data)
+  },
+  SETMESSAGE(state, data) {
+    state.messageList = data
+  }
 }
-const actions={
-    saveMessage({commit},data){  
-         commit('PUTMESSAGE',data)
-        
-    } 
+const actions = {
+  saveMessage({ commit }, data) {
+    commit('PUTMESSAGE', data)
+
+  }
 }
-export default{
-    state,mutations,actions
+export default {
+  state, mutations, actions
 }
