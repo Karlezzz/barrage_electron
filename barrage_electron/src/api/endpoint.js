ece43ca1404018236dd4a732abbcac47dc50c5a6
karle <970587844@qq.com>
2023-09-09 15:54:19 +0800

    add endpoint.js

---
+++ barrage_electron/src/api/endpoint.js
@@ -0,0 +1,16 @@
+const endpoint = {
+  socket: '/socket',
+  user: '/user',
+  client: '/client',
+  classRoom: '/classRoom',
+  room: '/room',
+  vote: '/vote',
+}
+
+module.exports = {
+  endpoint
+}
+
+export {
+  endpoint
+}
