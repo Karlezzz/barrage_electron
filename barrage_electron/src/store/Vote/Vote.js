65c05cbb9cfad6f7f476eff16708f30b6ca10d23
karle <970587844@qq.com>
2023-09-09 17:03:44 +0800

    add dummy data in vote store

--- barrage_electron/src/store/Vote/Vote.js
+++ barrage_electron/src/store/Vote/Vote.js
@@ -2,29 +2,42 @@ import { _findAll } from '@/api'
 import { endpoint } from '@/api/endpoint'
 
 const state = {
-	votes: [],
+  votes: [
+    {
+      id: 1,
+      content: '今晚吃什么',
+    },
+    {
+      id: 2,
+      content: '几点下课',
+    },
+    {
+      id: 3,
+      content: '有没有作业',
+    },
+  ],
 }
 const mutations = {
-	SETVOTES(state, data) {
-		state.votes.push(data)
-	},
+  SETVOTES(state, data) {
+    state.votes.push(data)
+  },
 }
 const actions = {
-	async getAllVotes({ commit }) {
-		try {
-			const result = await _findAll(endpoint.vote)
-			if (result) {
-				commit('SETVOTES', result)
+  async getAllVotes({ commit }) {
+    try {
+      const result = await _findAll(endpoint.vote)
+      if (result) {
+        commit('SETVOTES', result)
         return
-			}
-		} catch (error) {
-			console.log(error)
-		}
-	},
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  },
 }
 export default {
-	namespaced: true,
-	state,
-	mutations,
-	actions,
+  namespaced: true,
+  state,
+  mutations,
+  actions,
 }
