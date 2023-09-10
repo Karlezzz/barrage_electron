98ea9fa2756d460de2cb72066334e9e1d3a4f72f
karle <970587844@qq.com>
2023-09-10 13:22:23 +0800

    update Class

--- barrage_electron/lib/models/Score/Score.js
+++ barrage_electron/lib/models/Score/Score.js
@@ -1,5 +1,39 @@
+const { nanoid } = require("nanoid")
+const { ClassRoom } = require("../ClassRoom")
+const { User } = require("../User")
+
 class Score {
-  constructor() { }
+  constructor(options) {
+    options = options || {}
+    this.id = options.id || nanoid()
+    this.created = options.created || (new Date().valueOf())
+    this.value = options.value
+    this.creator = User.init(options.creator)
+    this.classRoom = ClassRoom.init(options.classRoom)
+  }
+
+  get isValid() {
+    return !!this.id && !!this.creator && !!this.value && !!this.classRoom
+  }
+
+  static init(options = {}) {
+    if (options instanceof Score) {
+      return options
+    }
+    const instance = new Score(options)
+    return instance.isValid ? instance : null
+  }
+
+  static initFromArray(arr = []) {
+    if (Array.isArray(arr)) {
+      return arr.map(this.init)
+    }
+    return []
+  }
+
+  static initOnlyValidFromArray(arr = []) {
+    return this.initFromArray(arr).filter((i) => i)
+  }
 }
 module.exports = {
   Score
