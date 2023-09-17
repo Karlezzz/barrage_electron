f1e3a35a646b5ff0d09f7a86126fffa354e0dfa9
karle <970587844@qq.com>
2023-09-17 21:15:14 +0800

    delete

--- barrage_electron/src/store/Vote/Vote.js
+++ barrage_electron/src/store/Vote/Vote.js
@@ -3,25 +3,6 @@ import { endpoint } from '@/api/endpoint'
 const { Vote } = require('../../../lib/models')
 
 const state = {
-  // votes: Vote.initFromArray([{
-  //   question: 'How to learn Vue?',
-  //   content: 'test',
-  //   duration: 60000,
-  //   voteOptions: [
-  //     {
-  //       optionValue: 'Online',
-  //       selectMembers: [{ name: 'Tom', id: '001' }, { name: 'Sam', id: '002' }]
-  //     },
-  //     {
-  //       optionValue: 'Book',
-  //       selectMembers: [{ name: 'Karle', id: '011' }, { name: 'Mary', id: '032' }, { name: 'Don', id: '031' }]
-  //     },
-  //     {
-  //       optionValue: 'Class',
-  //       selectMembers: [{ name: 'Joe', id: '005' }, { name: 'Yan', id: '036' }, { name: 'Perry', id: '056' }]
-  //     }
-  //   ]
-  // },]),
   votes: []
 }
 const mutations = {
