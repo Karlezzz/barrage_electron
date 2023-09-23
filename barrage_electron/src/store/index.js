24fe5b490698ed92a4630ba8ed5517a5d5c61212
karle <970587844@qq.com>
2023-09-23 17:22:14 +0800

    add user store

--- barrage_electron/src/store/index.js
+++ barrage_electron/src/store/index.js
@@ -5,11 +5,13 @@ import barrage from './Barrage/barrage'
 import room from './Room/room'
 import axios from './Axios/axios'
 import vote from './Vote/Vote'
+import user from './User/user'
 export default new Vuex.Store({
   modules: {
     axios,
     barrage,
     room,
-    vote
+    vote,
+    user
   },
 })
